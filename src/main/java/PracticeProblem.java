ccd4e0d733ce64ccd9eaad9673f2a45f3ab7d9fb
kavin-mohanadas-ics4u1-02-25 <kavin.mohanadas@student.tdsb.on.ca>
2025-04-08 22:39:20 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,26 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int x) {
+		if (x<0) {
+			x = -x;
+		}
+		if (x<10) {
+			return x;
+		}
+		else {
+			return (x%10) + sumOfDigits(x / 10);
+		}
+	}
 
-}
+	public static int factorial(int n) {
+		if (n<0) {
+			return 0;
+		}
+		if (n == 0) {
+			return 1;
+		}
+		return n*factorial(n-1);
+	}
+	}
+
